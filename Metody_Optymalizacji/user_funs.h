2c67336f2641ba4b0db9249f7db90239a023a981
Konrad4444 <konrad_warzecha@wp.pl>
2023-10-17 12:18:15 +0200

    df1

--- Metody_Optymalizacji/user_funs.h
+++ Metody_Optymalizacji/user_funs.h
@@ -6,3 +6,4 @@ matrix ff0T(matrix, matrix = NAN, matrix = NAN);
 matrix ff0R(matrix, matrix = NAN, matrix = NAN);
 matrix df0(double, matrix, matrix = NAN, matrix = NAN);
 matrix f1(matrix x, matrix = NAN, matrix = NAN);
+matrix df1(double, matrix, matrix);
